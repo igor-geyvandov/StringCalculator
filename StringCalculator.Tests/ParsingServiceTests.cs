f60ef22c6b96066a89a3b3354ba882e90093cb81
aspectsharp <igeyvandov@gmail.com>
2024-03-15 13:15:53 -0400

    Req 5

--- StringCalculator.Tests/ParsingServiceTests.cs
+++ StringCalculator.Tests/ParsingServiceTests.cs
@@ -43,12 +43,35 @@ namespace StringCalculator.Tests
         }
 
         [TestMethod]
+        [DataRow("1,-2,3", DisplayName = "'1,-2,3' = exception thrown")]
+        [DataRow("1,-2,3,4\n5,6,-7,8,9", DisplayName = "'1,-2,3,4\n5,6,-7,8,9' = exception thrown")]
+        public void ParsingService_Throws_Exception_When_Negative_Numbers_Provided(string input)
+        {
+            const int maxNumOfValuesAllowed = int.MaxValue;
+            string[] delimiters = new string[] { ",", "\n" };
+            ParsingService service = new ParsingService();
+            Assert.ThrowsException<Exception>(() => service.ParseInput(input, delimiters, maxNumOfValuesAllowed));
+        }
+
+        [TestMethod]
         [DataRow("1,2,3,4", new int[] { 1,2,3,4 }, DisplayName = "'1,2,3,4' = [1,2,3,4]")]
         [DataRow("1,2,3,4,5,6,7,8,9,10", new int[] { 1,2,3,4,5,6,7,8,9,10 }, DisplayName = "'1,2,3,4,5,6,7,8,9,10' = [1,2,3,4,5,6,7,8,9,10]")]
         [DataRow("1,2,a,4,5,6,7,$,9,10", new int[] { 1, 2, 0, 4, 5, 6, 7, 0, 9, 10 }, DisplayName = "'1,2,a,4,5,6,7,$,9,10' = [1,2,0,4,5,6,7,0,9,10]")]
         [DataRow("1\n2,3", new int[] { 1, 2, 3 }, DisplayName = "'1\n2,3' = [1, 2, 3]")]
         [DataRow("1\n2,3,4\n5", new int[] { 1, 2, 3, 4, 5 }, DisplayName = "'1\n2,3,4\n5' = [1, 2, 3, 4, 5]")]
         public void ParsingService_Returns_Numbers_Without_Constrain(string input, int[] expected)
+        {
+            const int maxNumOfValuesAllowed = int.MaxValue;
+            string[] delimiters = new string[] { ",", "\n" };
+            ParsingService service = new ParsingService();
+            CollectionAssert.AreEqual(expected, service.ParseInput(input, delimiters, maxNumOfValuesAllowed));
+        }
+
+        [TestMethod]
+        [DataRow("1,2,1001,4", new int[] { 1, 2, 0, 4 }, DisplayName = "'1,2,1001,4' = [1,2,0,4]")]
+        [DataRow("1,2\n1001,4", new int[] { 1, 2, 0, 4 }, DisplayName = "'1,2\n1001,4' = [1,2,0,4]")]
+        [DataRow("1,2\n1001,20000,5,999", new int[] { 1, 2, 0, 0, 5, 999 }, DisplayName = "'1,2\n1001,20000,5,999' = [1,2,0,0,5,999]")]
+        public void ParsingService_Returns_Numbers_While_Invalidating_Large_Numbers(string input, int[] expected)
         {
             const int maxNumOfValuesAllowed = int.MaxValue;
             string[] delimiters = new string[] { ",", "\n" };
--- StringCalculator/ParsingService.cs
+++ StringCalculator/ParsingService.cs
@@ -12,10 +12,17 @@
                 {
                     intValues = Array.ConvertAll(strValues, s => int.TryParse(s, out var i) ? i : 0);
                     
+                    //Throw exception for negative numbers
                     int[] negativeIntValues = intValues.Where(v => v < 0).ToArray();
                     if (negativeIntValues.Length > 0)
                     {
                         throw new Exception($"Negative numbers provided: {string.Join(",", negativeIntValues)}");
+                    }
+
+                    //Invalidate numbers greater than 1000
+                    for (int i = 0; i < intValues.Length; i++)
+                    {
+                        intValues[i] = intValues[i] > 1000 ? 0 : intValues[i];
                     }
                 }
                 else
